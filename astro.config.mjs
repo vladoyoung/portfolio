658f159870575fe112bc0bcaeb241a992e1f037e
Vlad Nikolov <vladoyoung@gmail.com>
2023-08-26 21:53:48 +0300

    Rework some content and add AtTbb snippet

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,6 +35,7 @@ export default defineConfig({
         "@/shortcodes/Youtube",
         "@/shortcodes/Tabs",
         "@/shortcodes/Tab",
+        "@/shortcodes/AtTbb.astro",
       ],
     }),
     mdx(),
